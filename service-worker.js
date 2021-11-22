b75a0080ab0cc8b15e2e8b81d3068ceb5a29ecfc
YuHyeon <jujini31@gmail.com>
2021-11-22 15:55:46 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-github-pages/precache-manifest.a2b326cac96ed75ef75007cf1e7aae9a.js"
+  "/react-github-pages/precache-manifest.ad0f121d9fdb9ab59d2ec64cc4ebe329.js"
 );
 
 self.addEventListener('message', (event) => {
